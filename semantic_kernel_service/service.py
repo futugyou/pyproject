067fb074b31896254b419104a7ac3c3b737481b7
futugyou <17871902+futugyou@users.noreply.github.com>
2025-07-27 16:30:42 +0000

    Added audio to text demo

--- semantic_kernel_service/service.py
+++ semantic_kernel_service/service.py
@@ -1,5 +1,8 @@
 from semantic_kernel import Kernel
-from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+from semantic_kernel.connectors.ai.open_ai import (
+    OpenAIChatCompletion,
+    OpenAIAudioToText,
+)
 from semantic_kernel.connectors.ai.google.google_ai import (
     GoogleAIEmbeddingPromptExecutionSettings,
     GoogleAITextEmbedding,
@@ -31,3 +34,12 @@ text_embedding_service = GoogleAITextEmbedding(
 kernel.add_service(text_embedding_service)
 
 memoryClient = MemoryClient(api_key=os.getenv("MEM0_API_KEY"))
+
+audio_to_text_service = OpenAIAudioToText(
+    ai_model_id=os.getenv("OPENAI_CHAT_MODEL_ID"),
+    service_id="audio_to_text",
+    async_client=AsyncOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url=os.getenv("OPENAI_URL"),
+    ),
+)
