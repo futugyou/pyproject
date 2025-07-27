4313fd42004346c9ec37a7991c4f6985fe12c5a2
futugyou <17871902+futugyou@users.noreply.github.com>
2025-07-27 17:42:40 +0000

    Added prompt cache use cosine distance to choose match propmt and result

--- semantic_kernel_service/v01_prompt.py
+++ semantic_kernel_service/v01_prompt.py
@@ -1,6 +1,107 @@
+import asyncio
+import time
+from collections.abc import Awaitable, Callable
+from dataclasses import dataclass, field
+from typing import Annotated
+from uuid import uuid4
+
 from semantic_kernel import Kernel
 from semantic_kernel.prompt_template import InputVariable, PromptTemplateConfig
-import asyncio
+from semantic_kernel.connectors.ai.open_ai import (
+    OpenAIChatCompletion,
+    OpenAITextEmbedding,
+)
+from semantic_kernel.connectors.in_memory import InMemoryStore
+from semantic_kernel.data.vector import (
+    VectorStore,
+    VectorStoreCollection,
+    VectorStoreField,
+    vectorstoremodel,
+)
+from semantic_kernel.filters import (
+    FilterTypes,
+    FunctionInvocationContext,
+    PromptRenderContext,
+)
+from semantic_kernel.functions import FunctionResult, KernelFunction
+
+COLLECTION_NAME = "llm_responses"
+RECORD_ID_KEY = "cache_record_id"
+
+
+# Define a simple data model to store, the prompt and the result
+# we annotate the prompt field as the vector field, the prompt itself will not be stored.
+# and if you use `include_vectors` in the search, it will return the vector, but not the prompt.
+@vectorstoremodel(collection_name=COLLECTION_NAME)
+@dataclass
+class CacheRecord:
+    result: Annotated[str, VectorStoreField("data", is_full_text_indexed=True)]
+    prompt: Annotated[str | None, VectorStoreField("vector", dimensions=1536)] = None
+    id: Annotated[str, VectorStoreField("key")] = field(
+        default_factory=lambda: str(uuid4())
+    )
+
+
+class PromptCacheFilter:
+    """A filter to cache the results of the prompt rendering and function invocation."""
+
+    def __init__(
+        self,
+        vector_store: VectorStore,
+        score_threshold: float = 0.2,
+    ):
+        if vector_store.embedding_generator is None:
+            raise ValueError("The vector store must have an embedding generator.")
+        self.vector_store = vector_store
+        self.collection: VectorStoreCollection[str, CacheRecord] = (
+            vector_store.get_collection(record_type=CacheRecord)
+        )
+        self.score_threshold = score_threshold
+
+    async def on_prompt_render(
+        self,
+        context: PromptRenderContext,
+        next: Callable[[PromptRenderContext], Awaitable[None]],
+    ):
+        """Filter to cache the rendered prompt and the result of the function.
+
+        It uses the score threshold to determine if the result should be cached.
+        The direction of the comparison is based on the default distance metric for
+        the in memory vector store, which is cosine distance, so the closer to 0 the
+        closer the match.
+        """
+        print("on_prompt_render")
+        await next(context)
+        await self.collection.ensure_collection_exists()
+        results = await self.collection.search(
+            context.rendered_prompt, vector_property_name="prompt", top=1
+        )
+        async for result in results.results:
+            if result.score is not None and result.score < self.score_threshold:
+                print("Cache hit, score:", result.score)
+                context.function_result = FunctionResult(
+                    function=context.function.metadata,
+                    value=result.record.result,
+                    rendered_prompt=context.rendered_prompt,
+                    metadata={RECORD_ID_KEY: result.record.id},
+                )
+
+    async def on_function_invocation(
+        self,
+        context: FunctionInvocationContext,
+        next: Callable[[FunctionInvocationContext], Awaitable[None]],
+    ):
+        """Filter to store the result in the cache if it is new."""
+        print("on_function_invocation")
+        await next(context)
+        result = context.result
+        if result and result.rendered_prompt and RECORD_ID_KEY not in result.metadata:
+            print("Cache miss")
+            cache_record = CacheRecord(
+                prompt=result.rendered_prompt, result=str(result)
+            )
+            await self.collection.ensure_collection_exists()
+            await self.collection.upsert(cache_record)
 
 
 prompt = """{{$input}}
@@ -28,12 +129,9 @@ Demo, like Julia Balbilla, writes in the artificial and poetic Aeolic dialect. T
 """
 
 
-async def generate_summarize(kernel: Kernel, input_text: str) -> str:
-    summarize = kernel.add_function(
-        function_name="summarizeFunc",
-        plugin_name="summarizePlugin",
-        prompt_template_config=prompt_template_config,
-    )
+async def generate_summarize(
+    kernel: Kernel, summarize: KernelFunction, input_text: str
+) -> str:
     summary = await kernel.invoke(summarize, input=input_text)
     return str(summary)
 
@@ -41,9 +139,23 @@ async def generate_summarize(kernel: Kernel, input_text: str) -> str:
 if __name__ == "__main__":
 
     async def main():
-        from service import kernel
+        from service import kernel, text_embedding_service
 
-        summary = await generate_summarize(kernel, input_text)
+        # create the in-memory vector store
+        vector_store = InMemoryStore(embedding_generator=text_embedding_service)
+        # create the cache filter and add the filters to the kernel
+        cache = PromptCacheFilter(vector_store=vector_store)
+        kernel.add_filter(FilterTypes.PROMPT_RENDERING, cache.on_prompt_render)
+        kernel.add_filter(FilterTypes.FUNCTION_INVOCATION, cache.on_function_invocation)
+
+        summarize = kernel.add_function(
+            function_name="summarizeFunc",
+            plugin_name="summarizePlugin",
+            prompt_template_config=prompt_template_config,
+        )
+        summary = await generate_summarize(kernel, summarize, input_text)
+        print(summary)
+        summary = await generate_summarize(kernel, summarize, input_text)
         print(summary)
 
     asyncio.run(main())
