e238ce440df709b48118a9d0c7c94f8c4822260e
futugyou <17871902+futugyou@users.noreply.github.com>
2025-08-05 15:41:19 +0000

    Use custom termination

--- autogen_demo/v07_single_team.py
+++ autogen_demo/v07_single_team.py
@@ -16,6 +16,7 @@ from autogen_core import CancellationToken
 from autogen_core.models import UserMessage, ModelFamily
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
+from function_call_termination import FunctionCallTermination
 
 load_dotenv()
 
@@ -23,6 +24,11 @@ load_dotenv()
 def increment_number(number: int) -> int:
     """Increment a number by 1."""
     return number + 1
+
+
+def approve() -> None:
+    """Approve the message when all feedbacks have been addressed."""
+    pass
 
 
 async def run() -> None:
@@ -46,9 +52,22 @@ async def run() -> None:
         tools=[increment_number],  # Register the tool.
         system_message="You are a helpful AI assistant, use the tool to increment the number.",
     )
+    primary_agent = AssistantAgent(
+        "primary",
+        model_client=model_client,
+        system_message="You are a helpful AI assistant.",
+    )
+    critic_agent = AssistantAgent(
+        "critic",
+        model_client=model_client,
+        tools=[approve],  # Register the approve function as a tool.
+        system_message="Provide constructive feedback. Use the approve tool to approve when all feedbacks are addressed.",
+    )
 
     # Termination condition that stops the task if the agent responds with a text message.
     termination_condition = TextMessageTermination("looped_assistant")
+
+    function_call_termination = FunctionCallTermination(function_name="approve")
 
     # Create a team with the looped assistant agent and the termination condition.
     team = RoundRobinGroupChat(
@@ -63,7 +82,17 @@ async def run() -> None:
     # ToolCallSummaryMessage
     # TextMessage
     # TaskResult
-    async for message in team.run_stream(task="Increment the number 5 to 10."):  # type: ignore
+    async for message in team.run_stream(task="Increment the number 5 to 7."):  # type: ignore
+        print(type(message).__name__, message)
+
+    team = RoundRobinGroupChat(
+        [primary_agent, critic_agent],
+        termination_condition=function_call_termination,
+    )
+
+    async for message in team.run_stream(
+        task="Write a unique, Haiku about the weather in Paris"
+    ):  # type: ignore
         print(type(message).__name__, message)
 
     await model_client.close()
