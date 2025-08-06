9fdb01380c8ed123a5995b5e5c0874fa717c9494
futugyou <17871902+futugyou@users.noreply.github.com>
2025-08-06 16:47:21 +0000

    Updated magentic one damo

--- autogen_demo/v12_magentic.py
+++ autogen_demo/v12_magentic.py
@@ -3,16 +3,20 @@ import os
 from dotenv import load_dotenv
 from typing import Any, Dict, List
 
-from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.agents import AssistantAgent, CodeExecutorAgent
 from autogen_agentchat.conditions import HandoffTermination, TextMentionTermination
 from autogen_agentchat.messages import HandoffMessage
 from autogen_agentchat.teams import MagenticOneGroupChat
 from autogen_agentchat.ui import Console
 from autogen_core.models import ModelFamily
+from autogen_ext.agents.file_surfer import FileSurfer
+from autogen_ext.agents.magentic_one import MagenticOneCoderAgent
+from autogen_ext.code_executors.local import LocalCommandLineCodeExecutor
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 
 load_dotenv()
+
 
 async def run() -> None:
     model_client = OpenAIChatCompletionClient(
@@ -32,8 +36,20 @@ async def run() -> None:
         "Assistant",
         model_client=model_client,
     )
-    team = MagenticOneGroupChat([assistant], model_client=model_client)
-    await Console(team.run_stream(task="Write a Python script to fetch data from an API."))
+
+    file_surfer = FileSurfer("FileSurfer", model_client=model_client)
+    coder = MagenticOneCoderAgent("Coder", model_client=model_client)
+    terminal = CodeExecutorAgent(
+        "ComputerTerminal", code_executor=LocalCommandLineCodeExecutor()
+    )
+    # team = MagenticOneGroupChat([assistant], model_client=model_client)
+
+    team = MagenticOneGroupChat(
+        [file_surfer, coder, terminal], model_client=model_client
+    )
+    await Console(
+        team.run_stream(task="Write a Python script to fetch data from an API.")
+    )
     await model_client.close()
 
 
