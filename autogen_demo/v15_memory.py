baea1537123675a7bc0b10bb40d7b58924995d76
futugyou <17871902+futugyou@users.noreply.github.com>
2025-08-07 15:46:49 +0000

    Added autogen memory demo

---
+++ autogen_demo/v15_memory.py
@@ -0,0 +1,67 @@
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.ui import Console
+from autogen_core.memory import ListMemory, MemoryContent, MemoryMimeType
+from autogen_core.models import ModelFamily
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+import asyncio
+import os
+from dotenv import load_dotenv
+from typing import Any, Dict, List
+
+load_dotenv()
+
+
+async def run() -> None:
+    model_client = OpenAIChatCompletionClient(
+        model=os.getenv("GOOGLE_CHAT_MODEL_ID"),
+        base_url=os.getenv("GOOGLE_URL"),
+        api_key=os.getenv("GOOGLE_API_KEY"),
+        model_info={
+            "vision": True,
+            "function_calling": True,
+            "json_output": True,
+            "family": ModelFamily.ANY,
+            "structured_output": True,
+            "multiple_system_messages": True,
+        },
+    )
+
+    # Initialize user memory
+    user_memory = ListMemory()
+
+    # Add user preferences to memory
+    await user_memory.add(
+        MemoryContent(
+            content="The weather should be in metric units",
+            mime_type=MemoryMimeType.TEXT,
+        )
+    )
+
+    await user_memory.add(
+        MemoryContent(
+            content="Meal recipe must be vegan", mime_type=MemoryMimeType.TEXT
+        )
+    )
+
+    async def get_weather(city: str, units: str = "imperial") -> str:
+        if units == "imperial":
+            return f"The weather in {city} is 73 °F and Sunny."
+        elif units == "metric":
+            return f"The weather in {city} is 23 °C and Sunny."
+        else:
+            return f"Sorry, I don't know the weather in {city}."
+
+    assistant_agent = AssistantAgent(
+        name="assistant_agent",
+        model_client=model_client,
+        tools=[get_weather],
+        memory=[user_memory],
+    )
+    # Run the agent with a task.
+    stream = assistant_agent.run_stream(task="What is the weather in New York?")
+    await Console(stream)
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
