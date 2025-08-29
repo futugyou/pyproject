e3b8b7fcf74e5bdea4a8039b50587e212354d459
futugyou <17871902+futugyou@users.noreply.github.com>
2025-08-29 16:44:19 +0000

    Added react agent implementation in langchain adapter with new prompt template

--- langchain_adapter/agent.py
+++ langchain_adapter/agent.py
@@ -4,6 +4,8 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
 
+from langgraph.prebuilt import create_react_agent
+
 from typing import Optional
 from pydantic import BaseModel, Field
 
@@ -14,6 +16,16 @@ prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "You are a helpful assistant"),
         ("human", "{input}"),
+        # Placeholders fill up a **list** of messages
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+)
+
+
+prompt2 = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant"),
+        ("placeholder", "{messages}"),
         # Placeholders fill up a **list** of messages
         ("placeholder", "{agent_scratchpad}"),
     ]
@@ -49,6 +61,19 @@ def calculate(query: str, config: LangChainOption):
     return response["output"]
 
 
+def calculate2(query: str, config: LangChainOption):
+    llm = init_chat_model(
+        config.lang_google_chat_model,
+        model_provider="google_genai",
+        api_key=config.lang_google_api_key,
+    )
+
+    agent = create_react_agent(llm, tools, prompt=prompt2)
+
+    response = agent.invoke({"messages": [("human", query)]})
+    return response["messages"][-1].content
+
+
 if __name__ == "__main__":
-    result = calculate("What is 3 * 12? Also, what is 11 + 49?", LangChainOption())
+    result = calculate2("What is 3 * 12? Also, what is 11 + 49?", LangChainOption())
     print(result)
