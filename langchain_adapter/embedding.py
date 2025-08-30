b4661fb67444ad175e7e55c9bdc4186d82ee8179
futugyou <17871902+futugyou@users.noreply.github.com>
2025-08-30 17:05:15 +0000

    Added document retrieval functionality in langchain adapter with FAISS vector store support

--- langchain_adapter/embedding.py
+++ langchain_adapter/embedding.py
@@ -1,4 +1,7 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import CharacterTextSplitter
 
 from typing import Any, Dict, List, Optional
 from pydantic import BaseModel, Field
@@ -14,15 +17,33 @@ def generate_embedding(input_texts: List[str], config: LangChainOption):
     return embeddings_model.embed_documents(input_texts)
 
 
-if __name__ == "__main__":
-    result = generate_embedding(
-        [
-            "Hi there!",
-            "Oh, hello!",
-            "What's your name?",
-            "My friends call me World",
-            "Hello World!",
-        ],
-        LangChainOption(),
+def retriever(path: str, config: LangChainOption):
+    embeddings_model = GoogleGenerativeAIEmbeddings(
+        model=config.lang_google_embedding_model,
+        google_api_key=config.lang_google_api_key,
     )
-    print(result)
+
+    loader = TextLoader(path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    vectorstore = FAISS.from_documents(texts, embeddings_model)
+    retriever = vectorstore.as_retriever()
+    return retriever.invoke("what is `Structure`?")
+
+
+if __name__ == "__main__":
+    # result = generate_embedding(
+    #     [
+    #         "Hi there!",
+    #         "Oh, hello!",
+    #         "What's your name?",
+    #         "My friends call me World",
+    #         "Hello World!",
+    #     ],
+    #     LangChainOption(),
+    # )
+    # print(result)
+
+    doc = retriever("./README.md", LangChainOption())
+    print(doc)
