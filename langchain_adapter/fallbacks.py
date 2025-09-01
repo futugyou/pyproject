a66ab838534f17d12c429047da977092cfbb2e68
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-01 14:29:31 +0000

    Added joke generation support in langchain adapter with OpenAI and fallback to Google Generative AI

---
+++ langchain_adapter/fallbacks.py
@@ -0,0 +1,48 @@
+from langchain.chat_models import init_chat_model
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+from typing import Optional
+from pydantic import BaseModel, Field
+
+from .option import LangChainOption
+
+
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") and the final punchline (the response to "<setup> who?").
+
+Here are some examples of jokes:
+
+example_user: Tell me a joke about planes
+example_assistant: {{"setup": "Why don't planes ever get tired?", "punchline": "Because they have rest wings!", "rating": 2}}"""
+
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{input}")])
+
+
+class Joke(BaseModel):
+    """Joke to tell user."""
+
+    setup: str = Field(description="The setup of the joke")
+    punchline: str = Field(description="The punchline to the joke")
+    rating: Optional[int] = Field(
+        default=None, description="How funny the joke is, from 1 to 10"
+    )
+
+
+def generate_joke(input_text: str, config: LangChainOption) -> Joke:
+    # If there is no 'export OPENAI_API_KEY', an error will be reported directly and fallback llm `gemini_llm` will not be triggered
+    openai_llm = ChatOpenAI(max_retries=0)
+    gemini_llm = init_chat_model(
+        config.lang_google_chat_model,
+        model_provider="google_genai",
+        api_key=config.lang_google_api_key,
+    )
+    llm = openai_llm.with_fallbacks([gemini_llm])
+    structured_llm = llm.with_structured_output(Joke)
+    few_shot_structured_llm = prompt | structured_llm
+    return few_shot_structured_llm.invoke(input_text)
+
+
+if __name__ == "__main__":
+    result = generate_joke("Tell me a joke about cats", LangChainOption())
+    print(result)
