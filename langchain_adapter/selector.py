7a0661188bd2581747d687115e60bbe541d47344
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-03 17:03:22 +0000

    Added MMR and semantic similarity example selectors in langchain adapter with GoogleGenerativeAIEmbeddings and FAISS integration

--- langchain_adapter/selector.py
+++ langchain_adapter/selector.py
@@ -1,12 +1,18 @@
 from langchain.chains import LLMChain
 from langchain.chat_models import init_chat_model
+from langchain_community.vectorstores import FAISS
 from langchain.prompts import (
     PromptTemplate,
     StringPromptTemplate,
     FewShotPromptTemplate,
 )
-from langchain.prompts.example_selector import LengthBasedExampleSelector
+from langchain.prompts.example_selector import (
+    MaxMarginalRelevanceExampleSelector,
+    SemanticSimilarityExampleSelector,
+    LengthBasedExampleSelector,
+)
 from langchain.prompts.example_selector.base import BaseExampleSelector
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 
 from typing import Dict, List
@@ -40,6 +46,8 @@ def custom_selector(config: LangChainOption):
 
     print(selector.select_examples({"foo": "foo"}))
 
+
+def length_selector(config: LangChainOption):
     examples = [
         {"input": "happy", "output": "sad"},
         {"input": "tall", "output": "short"},
@@ -74,5 +82,42 @@ def custom_selector(config: LangChainOption):
     print(dynamic_prompt.format(adjective="enthusiastic"))
 
 
+def mmr_selector(config: LangChainOption):
+    embedding = GoogleGenerativeAIEmbeddings(
+        model=config.lang_google_embedding_model,
+        google_api_key=config.lang_google_api_key,
+    )
+    example_prompt = PromptTemplate(
+        input_variables=["input", "output"],
+        template="Input: {input}\nOutput: {output}",
+    )
+
+    examples = [
+        {"input": "happy", "output": "sad"},
+        {"input": "tall", "output": "short"},
+        {"input": "energetic", "output": "lethargic"},
+        {"input": "sunny", "output": "gloomy"},
+        {"input": "windy", "output": "calm"},
+    ]
+
+    # example_selector = MaxMarginalRelevanceExampleSelector.from_examples(
+    example_selector = SemanticSimilarityExampleSelector.from_examples(
+        examples,
+        embedding,
+        FAISS,
+        k=3,
+    )
+    mmr_prompt = FewShotPromptTemplate(
+        example_selector=example_selector,
+        example_prompt=example_prompt,
+        prefix="Give the antonym of every input",
+        suffix="Input: {adjective}\nOutput:",
+        input_variables=["adjective"],
+    )
+    print(mmr_prompt.format(adjective="worried"))
+
+
 if __name__ == "__main__":
-    custom_selector(LangChainOption())
+    # custom_selector(LangChainOption())
+    # length_selector(LangChainOption())
+    mmr_selector(LangChainOption())
