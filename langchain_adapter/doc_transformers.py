99c16b6d2371afecb3fd88c5aeee50e79e2c17b0
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-05 09:06:55 +0000

    Added Markdown header text splitter support in langchain adapter with example

--- langchain_adapter/doc_transformers.py
+++ langchain_adapter/doc_transformers.py
@@ -9,6 +9,7 @@ from langchain_text_splitters import (
     CharacterTextSplitter,
     RecursiveCharacterTextSplitter,
     Language,
+    MarkdownHeaderTextSplitter,
 )
 
 
@@ -47,7 +48,25 @@ def python():
     print(python_docs)
 
 
+def markdown():
+    markdown_document = "# Foo\n\n    ## Bar\n\nHi this is Jim\n\nHi this is Joe\n\n ### Boo \n\n Hi this is Lance \n\n ## Baz\n\n Hi this is Molly"
+
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+    ]
+
+    markdown_splitter = MarkdownHeaderTextSplitter(
+        headers_to_split_on=headers_to_split_on
+    )
+    md_header_splits = markdown_splitter.split_text(markdown_document)
+    for split in md_header_splits:
+        print(split)
+
+
 if __name__ == "__main__":
     config = LangChainOption()
     # character_text_splitter()
-    python()
+    # python()
+    markdown()
