cadee011d14fbb57b9851cb701e92042309dac52
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-05 07:20:16 +0000

    Added JSON document loader support in langchain adapter with custom metadata extraction

--- langchain_adapter/doc_loader.py
+++ langchain_adapter/doc_loader.py
@@ -1,15 +1,27 @@
-
 import requests
-from langchain_community.document_loaders import CSVLoader, DirectoryLoader, TextLoader, PythonLoader, UnstructuredURLLoader,BSHTMLLoader,UnstructuredHTMLLoader
+from langchain_community.document_loaders import (
+    CSVLoader,
+    DirectoryLoader,
+    TextLoader,
+    PythonLoader,
+    UnstructuredURLLoader,
+    BSHTMLLoader,
+    UnstructuredHTMLLoader,
+    JSONLoader,
+)
 
 from .option import LangChainOption
 
+
 def csv():
-    loader = CSVLoader(file_path='./langchain_adapter/files/mlb_teams_2012.csv', csv_args={
-        'delimiter': ',',
-        'quotechar': '"',
-        'fieldnames': ['MLB Team', 'Payroll in millions', 'Wins']
-    })
+    loader = CSVLoader(
+        file_path="./langchain_adapter/files/mlb_teams_2012.csv",
+        csv_args={
+            "delimiter": ",",
+            "quotechar": '"',
+            "fieldnames": ["MLB Team", "Payroll in millions", "Wins"],
+        },
+    )
     data = loader.load()
     print(data)
 
@@ -17,12 +29,11 @@ def csv():
 def dir():
     # text_loader_kwargs={'autodetect_encoding': True}
     # loader = DirectoryLoader("", glob="**/*.py", loader_cls=TextLoader, loader_kwargs=text_loader_kwargs)
-    
-    
+
     loader = DirectoryLoader("./mcp_adapter", glob="**/*.py", loader_cls=PythonLoader)
     docs = loader.load()
     # print(docs)
-    doc_sources = [doc.metadata['source']  for doc in docs]
+    doc_sources = [doc.metadata["source"] for doc in docs]
     print(doc_sources)
 
 
@@ -41,12 +52,35 @@ def html():
     # loader = UnstructuredURLLoader(urls=[url])
     docs = loader.load()
     print(docs)
-    doc_sources = [doc.metadata['source']  for doc in docs]
+    doc_sources = [doc.metadata["source"] for doc in docs]
     print(doc_sources)
+
+
+def metadata_func(record: dict, metadata: dict) -> dict:
+    metadata["sender_name"] = record.get("sender_name")
+    metadata["timestamp_ms"] = record.get("timestamp_ms")
+
+    return metadata
+
+
+def json():
+    loader = JSONLoader(
+        file_path="./langchain_adapter/files/facebook_chat.json",
+        jq_schema=".messages[]",
+        content_key="content",
+        metadata_func=metadata_func,
+    )
+
+    data = loader.load()
+    # print(docs)
+    doc_sources = [doc.metadata["source"] for doc in docs]
+    print(doc_sources)
+
 
 if __name__ == "__main__":
     config = LangChainOption()
-    
+
     # csv()
     # dir()
-    html()
+    # html()
+    json()
