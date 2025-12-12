ec6110aa411c426591a01c5e46da9fbf9ae74c25
futugyou <17871902+futugyou@users.noreply.github.com>
2025-12-12 17:52:12 +0000

    Added JokeWithEmojis function to return jokes with emojis

--- agent_adapter/joke_agent.py
+++ agent_adapter/joke_agent.py
@@ -24,5 +24,25 @@ async def JokeAgent(query: str) -> str:
     return text
 
 
+async def JokeWithEmojis(query: str) -> list[str]:
+    messages: list[str] = []
+    client = client_factory.build_client("openai")
+
+    agent = client.create_agent(
+        instructions="You are good at telling jokes.", name="Joker"
+    )
+    thread = agent.get_new_thread()
+    result = await agent.run(query, thread=thread)
+    text = result.text
+    messages.append(text)
+    print(f"message: {text}\n")
+    result = await agent.run("Add emojis to previous jokes", thread=thread)
+    text = result.text
+    messages.append(text)
+    print(f"message: {text}\n")
+    return messages
+
+
 if __name__ == "__main__":
-    asyncio.run(JokeAgent("Tell me a joke about a pirate."))
+    # asyncio.run(JokeAgent("Tell me a joke about a pirate."))
+    asyncio.run(JokeWithEmojis("Tell me a joke about a pirate."))
