b21acd17d26e99f7b93e06d0a179222c64775581
futugyou <17871902+futugyou@users.noreply.github.com>
2025-12-18 18:00:20 +0000

    Added class based middleware

--- agent_adapter/middleware/agent.py
+++ agent_adapter/middleware/agent.py
@@ -1,5 +1,6 @@
 import logging
-from agent_framework import agent_middleware, AgentRunContext
+import time
+from agent_framework import AgentMiddleware, agent_middleware, AgentRunContext
 
 
 @agent_middleware
@@ -10,3 +11,23 @@ async def logging_agent_middleware(
     logging.info(f"Agent:{context.agent.name} starting...")
     await next(context)
     logging.info(f"Agent:{context.agent.name} finished!")
+
+
+class LogginAgentMiddleware(AgentMiddleware):
+    async def process(
+        self,
+        context: AgentRunContext,
+        next: Callable[[AgentRunContext], Awaitable[None]],
+    ) -> None:
+        logging.info(f"Agent:{context.agent.name} starting...")
+
+        context.metadata["some_tag"] = get_tracer().get_current_span().context.trace_id
+
+        start_time = time.time()
+
+        await next(context)
+
+        end_time = time.time()
+        duration = end_time - start_time
+
+        logging.info(f"Agent:{context.agent.name} completed in {duration:.5f}s.")
--- agent_adapter/middleware/function.py
+++ agent_adapter/middleware/function.py
@@ -1,5 +1,6 @@
 import logging
-from agent_framework import function_middleware, FunctionInvocationContext
+import time
+from agent_framework import FunctionMiddleware,function_middleware, FunctionInvocationContext
 
 
 @function_middleware
@@ -10,3 +11,22 @@ async def logging_function_middleware(
     logging.info(f"Function:{context.function.name} starting...")
     await next(context)
     logging.info(f"Function:{context.function.name} finished!")
+
+
+class LoggingFunctionMiddleware(FunctionMiddleware):
+    async def process(
+        self,
+        context: FunctionInvocationContext,
+        next: Callable[[FunctionInvocationContext], Awaitable[None]],
+    ) -> None:
+        function_name = context.function.name
+        logging.info(f"Function:{function_name} starting...")
+
+        start_time = time.time()
+
+        await next(context)
+
+        end_time = time.time()
+        duration = end_time - start_time
+
+        logging.info(f"Function {function_name} completed in {duration:.5f}s.")
